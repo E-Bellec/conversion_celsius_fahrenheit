72db7e4b21feccaca0d40ff8dbdfeca70060a378
Bellec Erwan <auto-entrepreneur@bellecerwan.com>
2017-11-14 19:04:32 +0100

    Function main
    * Print des choix utilisateurs pour le type de conversion
    * appel de la fonction getConversionTheUserWantToMake
    * Recupération de la reponse de l'utilisateur

--- conversion_celsius_fahrenheit/src/conversion_celsius_fahrenheit/ConversionCelsiusFahrenheit.java
+++ conversion_celsius_fahrenheit/src/conversion_celsius_fahrenheit/ConversionCelsiusFahrenheit.java
@@ -5,6 +5,49 @@ import java.util.Scanner;
 public class ConversionCelsiusFahrenheit {
 	
 	/**
+	 * Function main 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		boolean programClose = false;
+		Scanner scanner = new Scanner(System.in);
+		String conversion[] = {
+			"Conversion Celsius > Fahrenheit",
+			"Conversion Fahrenheit > Celsius"
+		};
+		
+		System.out.println(
+				"CONVERTISSEUR DEGRÉS CELSIUS ET DEGRÉS FAHRENHEIT\n"
+				+ "-------------------------------------------------\n"
+		);
+		
+		// As long as the user does not close the program
+		while ( !programClose ) {
+		
+			// We ask the user what conversion he wants to make
+			System.out.println(
+					"Quelle conversion souhaitez-vous faire ?\n"
+					+ "\t1 - " + conversion[0] + "\n"
+					+ "\t2 - " + conversion[1]
+			);
+		
+			// call of the function that retrieves the user input
+			String responseUser = getConversionTheUserWantToMake( scanner ); // return 1 or 2 only
+			
+			System.out.println(
+				responseUser
+			);
+			
+		} // END while program close = false
+		
+		System.out.println("Closure of the program");
+		scanner.close();
+		System.exit(0); // close the program without error
+			
+	} // END main
+	
+	/**
 	 * get type of conversion the user want to make
 	 * @return the conversion type 1 or 2 only
 	 */
